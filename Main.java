41091c5771853682731aa34cef6b2987989c837b
Muti-Kara <muti.kara@ug.bilkent.edu.tr>
2022-05-01 23:09:03 +0300

    CNN learning algorithm recreated.

--- Main.java
+++ Main.java
@@ -1,48 +1,61 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import image.data.MyImage;
+import image.io.MyImageIO;
 import neuralnet.algebra.Matrix;
-import neuralnet.network.net.ANN;
-import neuralnet.network.train.ANNTrainer;
+import neuralnet.network.net.CNN;
+import neuralnet.network.train.CNNTrainer;
 
 /**
 * Main
 */
 public class Main {
+	static String folderName = "/home/yuio/Desktop/OCR/data/";
+
+	static Matrix[] inputs;
+	static Matrix[] answers;
 	
-	public static void main(String[] args) {
+	static public int readFolder() throws IOException{
+		ArrayList<String> fileNames = new ArrayList<>();
+		for(char character = 'A'; character <= 'Z'; character++){
+			File[] files = new File(folderName + character).listFiles();
+			for(File file : files){
+				if(file.isFile())
+					fileNames.add(file.getName() + " " + character);
+			}
+		}
+		inputs = new Matrix[fileNames.size()];
+		answers = new Matrix[fileNames.size()];
+		for(int i = 0; i < fileNames.size(); i++){
+			String file = fileNames.get(i).substring(0, fileNames.get(i).length() - 2);
+			char character = fileNames.get(i).charAt(fileNames.get(i).length() - 1);
+			MyImage img = MyImageIO.read(folderName + character + "/" + file);
+			img.resize(27, 27);
+			inputs[i] = new Matrix(img.convertToDouble2D()).sProd(0.00002);
+			answers[i] = new Matrix(26, 1);
+			answers[i].set(character - 'A', 0, 1);
+		}
+		return fileNames.size();
+	}
+
+	public static void main(String[] args) throws IOException {
 		
-		// Matrix fat = new Matrix(new double[][] { {1, 1}, {0, 0} } );
-		// Matrix mot = new Matrix(new double[][] { {0, 0}, {1, 1} } );
+		readFolder();
 		
-		// Matrix[] childs = Matrix.breed(fat, mot, 10, 0.03);
+		CNN cnn = new CNN();
+		cnn.addLayer(CNN.CONVOLUTION, 2, 5);
+		cnn.addLayer(CNN.MAX_POOL, 2);
+		cnn.addLayer(CNN.CONVOLUTION, 2, 3);
+		cnn.addLayer(CNN.MAX_POOL, 2);
+		cnn.randomize(0.5);
+		inputs[0] = cnn.forwardPropagation(inputs[0]);
+		System.out.println(inputs[0]);
 		
-		// for (int i = 0; i < 10; i++) {
-		// 	System.out.println( childs[i] );
-		// }
-		
-		Matrix[] input = new Matrix[4];
-		input[0] = new Matrix(new double[][] { {1}, {1} });
-		input[1] = new Matrix(new double[][] { {1}, {0} });
-		input[2] = new Matrix(new double[][] { {0}, {1} });
-		input[3] = new Matrix(new double[][] { {0}, {0} });
-		
-		Matrix[] answer = new Matrix[4];
-		answer[0] = new Matrix(new double[][] { {1}, {0} });
-		answer[1] = new Matrix(new double[][] { {0}, {1} });
-		answer[2] = new Matrix(new double[][] { {0}, {1} });
-		answer[3] = new Matrix(new double[][] { {0}, {1} });
-		
-		ANN ann = new ANN();
-		ann.addLayer(ANN.SOFTMAX, 2, 2);
-		ann.randomize(0.01);
-		
-		ANNTrainer trainer = new ANNTrainer(ann, input, answer, 1);
-		trainer.train(20000, 100, 0.00001, 0.00003);
-		
-		System.out.println( ann.forwardPropagation( input[0] ) );
-		System.out.println( ann.forwardPropagation( input[1] ) );
-		System.out.println( ann.forwardPropagation( input[2] ) );
-		System.out.println( ann.forwardPropagation( input[3] ) );
-		
-		System.out.println(ann);
+		CNNTrainer trainer = new CNNTrainer(cnn, inputs, answers, 1);
+		trainer.setMiniTrainer(50, 10, 0.0001, 0.0003);
+		trainer.train(10, 5, 0, 0.03);
 		
 	}
 	
